a71313d8e4129ba75f28a9b637da4f0a0fe37032
John Doe <Igel101@web.de>
2015-07-23 00:19:22 +0200

    working graph

--- graph.cpp
+++ graph.cpp
@@ -4,12 +4,18 @@ struct Graph::Node {
 	int x = 0;
 	int y = 0;
 	std::string essid = "";
+	bool operator==(const Graph::Node& compareNode) const{
+		return(compareNode.x == x && compareNode.y == y && compareNode.essid == essid);
+	}
 };
 	
 struct Graph::Edge {
 	Node startNode;
 	Node endNode;
 	int weight = 0;
+	bool operator==(const Graph::Edge& compareEdge) const{
+		return(compareEdge.startNode == startNode && compareEdge.endNode == endNode);
+	}
 };
 	
 Graph::Node Graph::getNewNode(int x, int y, std::string essid) {
@@ -29,5 +35,5 @@ void Graph::createEdge(Node startNode, Node endNode) {
 }
 
 void Graph::deleteEdge(Edge removeEdge) {
-	edges.erase(std::find(edges.begin(), edges.end(), removeEdge));
+	edges.erase(std::remove(edges.begin(), edges.end(), removeEdge), edges.end());
 }
